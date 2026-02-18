e2023d326e5b324d4c759b7d6c213aecc8ddb420
damitymo <tymoszukdamiandejesus@gmail.com>
2026-02-17 21:21:34 -0300

    conexion a la DB

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,16 @@
 import server from './server';
 import { PORT } from './config/envs';
+import "reflect-metadata";
+import { AppDataSource } from './config/data-source';
 
-server.listen(PORT, () => {
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Conexion a la Base de Datos lograda con exito');
+        server.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
 
-    console.log(`Server is running on port ${PORT}`);
-});
+    })
+    .catch((error) => {
+        console.error('Error al conectar a la Base de Datos:', error);
+    });
