8c8ae0feb2237ccf894415b7c67c28123d0d9e8a
damitymo <tymoszukdamiandejesus@gmail.com>
2026-02-21 21:43:10 -0300

    creacion de entidad

---
+++ back/src/entities/User.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({
+    name: "users"
+})
+export class User {
+   @PrimaryGeneratedColumn()
+   id: number;
+
+   @Column({
+         length: 100
+   })
+   name: string;
+
+    @Column()
+    email: string;
+
+    @Column("integer")
+    age: number;
+
+    @Column()
+    isActive: boolean;
+ }
